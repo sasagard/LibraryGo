d8c46ed61ce85426f0eb1da2ade81dee6d4e10b9
Sebastian Sagardia <seba.sl@live.com>
2024-09-10 12:24:39 -0300

    creando clase database para conectar a mongodb y modificando estructuras con formato bson

---
+++ db/database.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func ConnectDB() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Conectado a BD")
+	return client
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return client.Database("LibroDB").Collection(collectionName)
+}
--- library/book.go
+++ library/book.go
@@ -1,7 +1,17 @@
 package library
 
 type Book struct {
-	Title  string
-	Author string
-	Year   int
+	Title  string `bson:"title"`
+	Author string `bson:"author"`
+	Year   int    `bson:"year"`
+}
+
+type PrintedBook struct {
+	Book  Book `bson:",inline"`
+	Pages int  `bson:"pages"`
+}
+
+type Ebook struct {
+	Book       Book `bson:",inline"`
+	FileSizeMB int  `bson:"fileSizeMB"`
 }
